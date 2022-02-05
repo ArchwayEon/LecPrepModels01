c1ac1a8164fe5579da86660c5d444e23fa466284
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2022-02-05 11:49:21 -0500

    Add project files.

---
+++ LecPrepModels01/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LecPrepModels01.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LecPrepModels01.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Person> People => Set<Person>();
+}
+
+
---
+++ LecPrepModels01/Services/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using LecPrepModels01.Models.Entities;
+
+namespace LecPrepModels01.Services;
+
+public interface IPersonRepository
+{
+    ICollection<Person> ReadAll();
+    Person Create(Person newPerson);
+    Person? Read(int id);
+    void Update(int oldId, Person person);
+    void Delete(int id);
+}
+
